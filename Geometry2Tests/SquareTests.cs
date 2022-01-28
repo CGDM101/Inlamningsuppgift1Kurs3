a92734f7e89c74d5cde1eef6199a7e827076af5e
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-01-28 20:05:22 +0100

    Added all tests with Calculator.cs.

--- Geometry2Tests/SquareTests.cs
+++ Geometry2Tests/SquareTests.cs
@@ -20,6 +20,62 @@ namespace Geometry2.Tests
 
             Assert.AreEqual(sut, 100);
         }
+
+        [TestMethod()]
+        public void TestRectangleAreaWithCalculator()
+        {
+            Calculator calc = new Calculator();
+            Rectangle myRectangle = new Rectangle(50);
+            myRectangle.Width = 20;
+
+            var sut = calc.GetRectangleArea(myRectangle);
+
+            Assert.AreEqual(sut, 1000);
+        }
+
+        [TestMethod()]
+        public void TestCircleAreaWithCalculator()
+        {
+            Calculator calc = new Calculator();
+            Circle myCircle = new Circle(10);
+
+            double sut = Math.Round(calc.GetCircleArea(myCircle), 1); // obs double
+
+            Assert.AreEqual(sut, 31.4);
+        }
+
+        [TestMethod()]
+        public void TestSquarePerimeterWithCalculator()
+        {
+            Calculator calc = new Calculator();
+            Square mySquare = new Square(10);
+
+            var sut = calc.GetSquarePerimeter(mySquare);
+
+            Assert.AreEqual(sut, 40);
+        }
+
+        [TestMethod()]
+        public void TestRectanglePerimeterWithCalculator()
+        {
+            Calculator calc = new Calculator();
+            Square mySquare = new Square(10);
+
+            var sut = calc.GetSquareArea(mySquare);
+
+            Assert.AreEqual(sut, 100);
+        }
+
+        [TestMethod()]
+        public void TestCirclePerimeterWithCalculator()
+        {
+            Calculator calc = new Calculator();
+            Circle myCircle = new Circle(10);
+
+            var sut = Math.Round(calc.GetCirclePerimeter(myCircle), 1);
+
+            Assert.AreEqual(sut, 78.5);
+        }
         #endregion 
 
         #region Utan Calculator.cs
